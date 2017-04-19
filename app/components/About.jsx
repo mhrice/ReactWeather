4058d506a87b1b747e38ef7070ca72157cbc5762
Matthew Rice <matthewrice11@yahoo.com>
2017-04-19 00:01:26 -0700

    Styled About page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,11 +1,32 @@
 var React = require('react');
-
+var {Link} = require('react-router');
 
 var About = (props) => {
   return(
     <div>
-  <h3>About</h3>
-  <p> This is the about page </p>
+  <h1 className="text-center">About</h1>
+  <div className="row">
+    <div className="columns medium-12 large-12 small-6">
+<ul>
+<li>
+  <div className="callout">
+  <h5>This is a callout.</h5>
+  <p>It has an easy to override visual style, and is appropriately subdued.</p>
+<a href="https://github.com/mhrice/ReactWeather">Github Repo</a>
+</div>
+</li>
+<li>
+<div className="callout secondary">
+<h5>This is a callout.</h5>
+<p>It has an easy to override visual style, and is appropriately subdued.</p>
+<a href="https://openweathermap.org">Open Weather Map</a> I used Open Weather Map
+to seach for weather data by city name
+</div>
+</li>
+
+</ul>
+</div>
+</div>
 </div>
 )
 };
